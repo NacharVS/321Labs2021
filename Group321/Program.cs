b10f9100322518ae0db21c8fc936fe1e79f85010
r3dinc <r3dinc@yandex.ru>
2021-09-07 13:09:29 +0300

    test commit

--- Group321/Program.cs
+++ Group321/Program.cs
@@ -6,7 +6,7 @@ namespace Group321
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
         }
     }
 }
