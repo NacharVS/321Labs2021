b9fa929d05788994345f68ec2c0194490c41939d
алина алина <fkbyf-2003zv@mail.ru>
2021-09-07 13:19:21 +0300

    Selection Bank

---
+++ Group321/Bank/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    class Account
+    {
+        private double _balanse;
+
+        public double Balanse
+        {
+            get => _balanse;
+            set
+            {
+                _balanse = value;
+            }
+        }
+    }
+}
