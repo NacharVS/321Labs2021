af51f13851f00c1eb3b8cc11f5260d9917643b39
Владислав Хлыбов <vavan8765@gmail.com>
2021-09-07 14:58:38 +0300

    Personal data

---
+++ Group321/PersonalData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group321
+{
+    struct PersonalData
+    {
+        string name;
+        string surname;
+        string patronymic;
+        string adress;
+        string idaccount;
+        string age;
+        Account acc;
+
+        public PersonalData(string name, string surname, string patronymic, string adress, string idaccount, string age, Account acc)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.patronymic = patronymic;
+            this.adress = adress;
+            this.idaccount = idaccount;
+            this.age = age;
+            this.acc = acc;
+        }
+    }
+}
