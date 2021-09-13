f3c0a51f86a6b0ef35bc0d685bce9a712e5b1e29
r3dinc <r3dinc@yandex.ru>
2021-09-13 09:42:13 +0300

    balance test

--- Group321/Program.cs
+++ Group321/Program.cs
@@ -6,7 +6,19 @@ namespace Group321
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            double balance = double.Parse(Console.ReadLine());
+
+            double rate = double.Parse(Console.ReadLine());
+
+            int mounthCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < mounthCount; i++)
+            {
+                balance = balance + balance * rate;
+            }
+            Console.WriteLine($"ur ballance {balance} after {mounthCount} mounths");
+
+
         }
     }
 }
