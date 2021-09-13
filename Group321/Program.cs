cd0d7301183f00f8abc553f6e9c800c3328bb7b2
r3dinc <r3dinc@yandex.ru>
2021-09-13 09:52:21 +0300

    fix

--- Group321/Program.cs
+++ Group321/Program.cs
@@ -5,20 +5,7 @@ namespace Group321
     class Program
     {
         static void Main(string[] args)
-        {
-            double balance = double.Parse(Console.ReadLine());
-
-            double rate = double.Parse(Console.ReadLine());
-
-            int mounthCount = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < mounthCount; i++)
-            {
-                balance = balance + balance * rate;
-            }
-            Console.WriteLine($"ur ballance {balance} after {mounthCount} mounths");
-
-
+        {           
         }
     }
 }
