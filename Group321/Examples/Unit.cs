9ea159f93119e8eb484798c1b8647cfb9dd358b5
r3dinc <r3dinc@yandex.ru>
2021-09-27 10:44:57 +0300

    events draft

--- Group321/Examples/Unit.cs
+++ Group321/Examples/Unit.cs
@@ -3,7 +3,9 @@
 namespace Group321.Examples
 {
     class Unit
-    {      
+    {
+        public delegate void ValueChangedDelegate(string message);
+
         private int _health = 100;
         private int _speed = 20;
 
@@ -12,7 +14,26 @@ namespace Group321.Examples
             get => _health;
             set
             {
-                _health = value;
+                var diff = _health - value;
+                if (_health > 0)
+                {
+                    if (_health < diff)
+                    {
+                        _health = 0;
+                        ValueChangedEvent?.Invoke($"Health chahded by {diff} current Health - {_health}");
+                        DeathEvent?.Invoke();
+                    }
+                    else
+                    {
+                        _health = value;
+                        ValueChangedEvent?.Invoke($"Health chahded by {diff} current Health - {_health}");
+                    }
+                }
+                else
+                {
+                    _health = 0;
+                    DeathEvent?.Invoke();
+                }
             }
         }
 
@@ -25,6 +46,12 @@ namespace Group321.Examples
             }
         }
 
+        public void InflictDamage(int damage)
+        {
+            Health -= damage;
+            Console.WriteLine($"Inflicted {damage} damage");
+        }
+
         public void Move()
         {
             Console.WriteLine("is moving");
@@ -34,6 +61,9 @@ namespace Group321.Examples
         {
             Console.WriteLine("making activity");
         }
+
+        public event ValueChangedDelegate ValueChangedEvent;
+        public event Action DeathEvent;
 
     }
 }
--- Group321/Program.cs
+++ Group321/Program.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using Group321.Examples;
+using System;
 
 namespace Group321
 {
     class Program
     {
+        delegate void MessageDelegate(string message);
+
         static void Main(string[] args)
-        {           
+        {
+            Unit unit = new Unit();
+            unit.ValueChangedEvent += ShowInfo;
+            unit.DeathEvent += Death;
+            unit.InflictDamage(10);
+            unit.InflictDamage(10);
+            unit.InflictDamage(40);
+            unit.InflictDamage(50);
+            unit.InflictDamage(50);
+        }
+
+        static void Death()
+        {
+            Console.WriteLine("Is dead");
+        }
+
+        static void ShowInfo(string info)
+        {
+            Console.WriteLine(info);
         }
     }
 }
