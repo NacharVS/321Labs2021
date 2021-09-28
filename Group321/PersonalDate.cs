aefa256d76dfac05b4fee13f2eaf8b2622bdf5a7
Марсель Баринов <barinov.marsel1111@gmail.com>
2021-09-28 12:06:28 +0300

    Made class with personal date of employeers

---
+++ Group321/PersonalDate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group321
+{
+    class PersonalDate
+    {
+        private string _FName, _LName, _TNumber, _BDay;
+        public PersonalDate(string FName, string LName, string TNumber,string BDay)
+        {
+            _FName = FName;
+            _LName = LName;
+            _TNumber = TNumber;
+            _BDay = BDay;
+        }
+    }
+}
