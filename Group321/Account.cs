98d8c73a6f374914e59c5abc4cb24319a75dcb2b
Анатолий Жмышенко <gggame184@gmail.com>
2021-09-30 06:12:14 +0300

    Made the initial menu and data actions

--- Group321/Account.cs
+++ Group321/Account.cs
@@ -6,5 +6,33 @@ namespace Group321
 {
     class Account
     {
+        private double bballance;
+
+        public double Ballance
+        {
+            get => bballance;
+            set
+            {
+                bballance = value;
+            }
+        }
+
+        public string idAccount
+        {
+            get;
+            set;
+        }
+
+        public double Percent
+        {
+            get;
+            set;
+        }
+
+        public double Telephone
+        {
+            get;
+            set;
+        }
     }
 }
--- Group321/Program.cs
+++ Group321/Program.cs
@@ -5,7 +5,30 @@ namespace Group321
     class Program
     {
         static void Main(string[] args)
-        {           
+        {
+            Account bank = new Account
+            {
+                idAccount = "123",
+                Telephone = 89392847361,
+                Ballance = 15000,
+                Percent = 0.9
+            };
+
+            Console.WriteLine("Выберите пункт");
+            Console.WriteLine("1 Зайти на счет cотрудника");
+            Console.WriteLine("2 Зайти на счет cотрудника");
+
+            string qwe = Console.ReadLine();
+
+            switch (qwe)
+            {
+                case "1":
+                    Console.WriteLine("Сотрудник, добро пожаловать!");
+                    break;
+                case "2":
+                    Console.WriteLine("Клиент, добро пожаловать!");
+                    break;
+            }
         }
     }
 }
