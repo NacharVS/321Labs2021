7cf75561a3b8ecfddfc8e813fea4377429e6d930
Марсель Баринов <barinov.marsel1111@gmail.com>
2021-09-30 10:21:55 +0300

    Added Interface

---
+++ Group321/Interface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group321
+{
+    interface Interface
+    {
+        void ShowBalance();
+        void Transaction();
+
+    }
+}
