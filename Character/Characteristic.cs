31989f1eaf779002c1aa971a6d939843306b6a2d
Гизетдинов Искандер <gizetiskander@gmail.com>
2021-09-30 11:08:01 +0300

    i create new project Character

---
+++ Character/Characteristic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character
+{
+    class Characteristic
+    {
+        public double Strength
+        {
+            get; set;
+        }
+
+        public double Agility
+        {
+            get; set;
+        }
+
+        public double Intelligence
+        {
+            get; set;
+        }
+
+        public double Physique
+        {
+            get; set;
+        }
+
+
+    }
+}
