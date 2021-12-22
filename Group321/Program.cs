027e350a85fefa2ddd761994f1bbf6cb86f42faf
Александр Краснов <krsasha200393@gmail.com>
2021-12-22 12:09:45 +0300

    Промежуточные результаты

--- Group321/Program.cs
+++ Group321/Program.cs
@@ -1,26 +1,62 @@
-﻿using Group321.Examples;
-using System;
+﻿using System;
+using System.Collections.Generic;
 
-namespace Group321
+namespace KrasnovExsam
 {
     class Program
     {
-        delegate void MessageDelegate(string message);
-
         static void Main(string[] args)
         {
-            IAction psnt = new Peasant(30);
-            psnt.Action();
+            List<Student> lst = new List<Student>()
+            {
+                new Student("Иванов", 5, 5, 5, 4, 3),
+                new Student("Краснов", 5, 5, 3, 4, 3),
+                new Student("Петров", 5, 2, 2, 5, 3),
+                new Student("Сергеев", 5, 5, 2, 4, 3),
+                new Student("Хлыбов", 2, 2, 2, 2, 3)
+            };
+            GetStudents(lst);
+        }
 
-        }
-        static void Death()
+        static void GetStudents(List<Student> students)
         {
-            Console.WriteLine("Is dead");
+            Console.WriteLine("Введит значение средней переменной оценки студента : ");
+            double Srednyia = Convert.ToDouble(Console.ReadLine());
+            foreach (var item in students)
+            {
+                if (item.SrednayOcenka >= Srednyia)
+                {
+                    Console.WriteLine("Средняя оценка более" + " " + Srednyia +  " у студентов : " + item.FIO );
+                }
+            }
+        }
+    }
+
+    public class Student
+    {
+        public string FIO;
+        int Russion;
+        int Ximico;
+        int Math;
+        int Phys;
+        int Fizika;
+       public double SrednayOcenka;
+
+        public Student(string fio, int Russion, int Ximico, int Math, int Phys, int Fizika)
+        {
+            this.Russion = Russion;
+            this.Ximico = Ximico;
+            this.Math = Math;
+            this.Phys = Phys;
+            this.Fizika = Fizika;
+            FIO = fio;
+            GetSred();
         }
 
-        static void ShowInfo(string info)
+        public void GetSred()
         {
-            Console.WriteLine(info);
+            SrednayOcenka = (Russion + Ximico + Math + Phys + Fizika) / 5;
         }
+
     }
 }
