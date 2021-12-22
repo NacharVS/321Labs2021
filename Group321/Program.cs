7cbd9b480319ec423b6a122c5fdf05b34dad723b
arullintalgat36@gmail.com <arullintalgat36@gmail.com>
2021-12-22 10:44:22 +0300

    add class singer

--- Group321/Program.cs
+++ Group321/Program.cs
@@ -1,26 +1,9 @@
-﻿using Group321.Examples;
-using System;
+﻿using System;
 
 namespace Group321
 {
     class Program
     {
-        delegate void MessageDelegate(string message);
-
-        static void Main(string[] args)
-        {
-            IAction psnt = new Peasant(30);
-            psnt.Action();
-
-        }
-        static void Death()
-        {
-            Console.WriteLine("Is dead");
-        }
-
-        static void ShowInfo(string info)
-        {
-            Console.WriteLine(info);
-        }
+        
     }
 }
---
+++ Group321/Singer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group321
+{
+    class Singer
+    {
+        public string Label { get; private set; }
+        public string Style { get; private set; }
+        public string Composition { get; private set; }
+        public string Album { get; private set; }
+        public int Year { get; private set; }
+
+        public Singer(string label, string style, string composition, string album, int year)
+        {
+            Label = label;
+            Style = style;
+            Composition = composition;
+            Album = album;
+            Year = year;
+        }
+    }
+}
