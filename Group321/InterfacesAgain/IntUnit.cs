8c0ee67e59f8e8c3d203fcc718fadf2f9922f738
булат шакиров <a2009-1@mail.ru>
2022-02-05 18:44:44 +0300

    add interface unit

---
+++ Group321/InterfacesAgain/IntUnit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group321.InterfacesAgain
+{
+    public interface IntUnit
+    {
+        public string Name { get; set; }
+        public double Hp { get; set; }
+        public double Damage { get; set; }
+        public double Armor { get; set; }
+        public double MoveSpeed { get; set; }
+    }
+}
---
+++ Group321/InterfacesAgain/Program.cs
@@ -0,0 +1,28 @@
+﻿using Group321.InterfacesAgain;
+using Group321.InterfacesAgain.Units;
+using Group321.InterfacesAgain.Weapons;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Group321
+{
+    class Program : Barracks
+    {
+        delegate void MessageDelegate(string message);
+
+        static void Main(string[] args)
+        {
+            Shaman Victor = new Shaman();
+            Blacksmith Igor = new Blacksmith();
+            Archer Amma = new Archer();
+            Soldier Andrew = new Soldier();
+            Victor.resurrection();
+            Victor.SoldierBuff();
+            var bow = Igor.MakeBow();
+            Amma.Hit(bow);
+        }
+
+    }
+}
